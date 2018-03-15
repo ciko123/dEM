cb78011ff91ef2f19831c354fce7da86a77bdf0b
mumian <mumian@2dfire.com>
2018-03-15 22:41:05 +0800

    [CHG] 代码梳理

---
+++ app/src/main/java/com/ciko/guo/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ciko.guo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ciko.guo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+
+    }
+}
